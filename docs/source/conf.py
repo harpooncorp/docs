a49ddc411b4589d8836b6cad173faa0302905cfd
Dominic Holt <dom@harpooncorp.io>
2022-03-17 23:25:23 -0700

    update config

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -2,9 +2,9 @@
 
 # -- Project information
 
-project = 'Lumache'
-copyright = '2021, Graziella'
-author = 'Graziella'
+project = 'harpoon'
+copyright = '2022, harpoon Corp'
+author = 'Dominic Holt'
 
 release = '0.1'
 version = '0.1.0'
